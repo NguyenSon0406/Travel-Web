0f0f330cb77953c06fd5dfcb6527e8e3a5d7368a
NguyenSon0406 <nguyenson062001@gmail.com>
2022-08-19 10:45:43 +0700

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+import React from 'react';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards';
+
+function Home () {
+    return (
+        <>
+            <HeroSection/>
+            <Cards/>
+            
+        </>
+    );
+};
+
+export default Home;
